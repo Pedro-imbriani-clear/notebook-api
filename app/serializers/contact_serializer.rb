776b9f4162293029855e149fdb9c5287827dbfe5
Pedro Imbriani <pedro.imbriani.clear@gmail.com>
2023-06-02 08:37:05 -0300

    Links and foreman

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -1,21 +1,26 @@
 class ContactSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :birthdate # , :author
 
-  belongs_to :kind
+  belongs_to :kind do
+    link(:related) { kind_url(object.kind.id)}
+  end
   has_many :phones
   has_one :address
 
- # def author
+  # link(:self) { contact_path(object.id) }
+  # link(:kind) { kind_url(object.id) }
+
+  # def author
   #  'Pedro Imbriani'
- # end
+  # end
 
   meta do
-    { author: "Pedro Imbriani"}
+    { author: 'Pedro Imbriani'}
   end
 
   def attributes(*args)
     h = super(*args)
-   # h[:birthdate] = (I18n.l(object.birthdate) unless object.birthdate.blank?)
+    # h[:birthdate] = (I18n.l(object.birthdate) unless object.birthdate.blank?)
     h[:birthdate] = object.birthdate.to_time.iso8601 unless object.birthdate.blank?
     h
   end
