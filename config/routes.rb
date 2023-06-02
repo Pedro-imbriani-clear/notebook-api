926a9981834e7adc847618369ce57cf53dbc2fc0
Pedro Imbriani <pedro.imbriani.clear@gmail.com>
2023-06-02 10:46:57 -0300

    CRUD with has_many and has_one

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,14 @@ Rails.application.routes.draw do
     resource :kind, only: [:show]
     resource :kind, only: [:show], path: 'relationships/kind'
 
-    resource :phones, only: [:show]
     resource :phones, only: [:show], path: 'relationships/phones'
+    resource :phones, only: [:show]
 
+    resource :phone, only: [:update, :create, :destroy]
+    resource :phone, only: [:update, :create, :destroy], path: 'relationships/phones'
 
-    resource :address, only: [:show]
-    resource :address, only: [:show], path: 'relationships/address'
+    resource :address, only: [:show, :update, :create, :destroy]
+    resource :address, only: [:show, :update, :create, :destroy], path: 'relationships/address'
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
